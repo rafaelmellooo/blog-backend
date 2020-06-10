e2ba8f14b23ca3634c3ab8b6f5bfb95e566b2b3a
Rafael Mello <rafaelmello0715@outlook.com>
2020-06-10 01:49:10 -0300

    feat: add test suites to posts service and posts controller

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { PostsController } from './posts.controller';
 import { PostsService } from './posts.service';
+import { PostsController } from './posts.controller';
 import { Post } from './post.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Post])],
-  controllers: [PostsController],
   providers: [PostsService],
+  controllers: [PostsController],
 })
 export class PostsModule {}
